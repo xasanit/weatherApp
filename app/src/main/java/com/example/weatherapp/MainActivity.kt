108c3af97b3c2602b81badcf60a18626b68f1718
xasanit <erlikhasanov@gmail.com>
2025-01-13 21:03:57 +0600

    content Page

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.example.weatherapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.weatherapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+
+        binding.confirmButton.setOnClickListener {
+
+        }
+    }
+}
